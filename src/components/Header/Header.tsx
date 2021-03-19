75e918182c159a82b957ae13ee64b4e05718b46c
Albeiro Marin <xlandru@hotmail.com>
2021-03-19 16:27:25 -0500

    Creates some parts of the header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './0px-599px.scss';
+import './600px-1024px.scss';
+import './1025px-1920px.scss';
+
+
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+
+            <div className='header__contact-bar'>
+
+            </div>
+
+        </header>
+    )
+};
+
+export default Header;
--- src/routes/MainContainer/MainContainer.tsx
+++ src/routes/MainContainer/MainContainer.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './0px-599px.scss';
 import './600px-1024px.scss';
 import './1025px-1920px.scss';
+import Header from '../../components/Header/Header'
 
 
 const MainContainer = () => {
 
     return (
         <div className='main-container'>
+
+            <Header />
 
         </div>
     )
