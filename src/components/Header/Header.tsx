9af2d215715ceca59375a2d63c5da9aa876a4f15
Albeiro Marin <xlandru@hotmail.com>
2021-03-30 08:21:08 -0500

    Add the brand logo to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,34 @@
-import React from 'react';
-import './0px-599px.scss';
-import './600px-1024px.scss';
-import './1025px-1920px.scss';
-
-
+import React from "react";
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+import logo from '../../assets/logo/brand.png';
 
 const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__contact-bar">
+        <div className="header__contact-data">
+          <p className="header__green-text">Contact us</p>
+          <p className="header__black-text">+420 336 775 664</p>
+          <p className="header__black-text">info@freshnesecom.com</p>
+          <div className="header__line"></div>
+        </div>
 
-    return (
-        <header className='header'>
+        <div className="header__contact-links">
+          <p className="header__green-link">Blog</p>
+          <p className="header__green-link">About Us</p>
+          <p className="header__green-link">Shipping Policy</p>
+        </div>
+      </div>
 
-            <div className='header__contact-bar'>
-
-                <div className='header__contact-data'>
-
-                    <p className='header__green-text'>Contact us</p>
-                    <p className='header__black-text'>+420 336 775 664</p>
-                    <p className='header__black-text'>info@freshnesecom.com</p>
-                    <div className='header__line'></div>
-                    
-                </div>
-
-                <div className='header__contact-links'>
-
-                    <p className='header__green-link'>Blog</p>
-                    <p className='header__green-link'>About Us</p>
-                    <p className='header__green-link'>Shipping Policy</p>
-                    
-                </div>
-
-            </div>
-
-            <div className='header__logo-bar'></div>
-
-        </header>
-    )
+      <div className="header__logo-bar">
+          <div className="header__logo-container">
+              <img className="header__logo-image" src={logo} alt="logo"/>
+          </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
