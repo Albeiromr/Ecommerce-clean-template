240b617ea2aff26a8eedf92a68a6cb663c247475
Albeiro Marin <xlandru@hotmail.com>
2021-04-20 17:53:43 -0500

    Add the footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__subcontainer">
+        <article className="footer__article"></article>
+        <div className='footer__social'></div>
+        <div className='footer__copyright'></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/routes/MainContainer/MainContainer.tsx
+++ src/routes/MainContainer/MainContainer.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import './0px-599px.scss';
-import './600px-1024px.scss';
-import './1025px-1920px.scss';
-import Header from '../../components/Header/Header';
-import MainNav from '../../components/Main nav/MainNav';
-import FixedMenu from '../../components/Fixed menu/FixedMenu';
-
+import React from "react";
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+import Header from "../../components/Header/Header";
+import MainNav from "../../components/Main nav/MainNav";
+import FixedMenu from "../../components/Fixed menu/FixedMenu";
+import Footer from "../../components/Footer/Footer";
 
 const MainContainer = () => {
+  return (
+    <div className="main-container">
+      <Header />
+      <MainNav />
 
-    return (
-        <div className='main-container'>
-
-            <Header />
-            <MainNav />
-
-            <FixedMenu/>
-
-
-        </div>
-    )
+      <Footer />
+      <FixedMenu />
+    </div>
+  );
 };
 
 export default MainContainer;
