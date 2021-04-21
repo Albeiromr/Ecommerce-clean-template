8afa229ef94354110bcd3e905be9d954b8216d4a
Albeiro Marin <xlandru@hotmail.com>
2021-04-21 10:01:44 -0500

    Add the footer subcontainer element with its text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,20 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__subcontainer">
-        <article className="footer__article"></article>
-        <div className='footer__social'></div>
-        <div className='footer__copyright'></div>
+        <article className="footer__article">
+          <div className="footer__article-subcontainer">
+            <div className="footer__article-text-box">
+              <p className="footer__article-title">All UPS FOR GRABS</p>
+              <h2 className="footer__article-subtitle">Enjoy up to 70% off</h2>
+              <p className="footer__article-paragraph">Lorem ipsum dolor amet lorem ipsum dolor amet aler products </p>
+              <div className="footer__article-button">
+                <p className="footer__article-button-text">View Products</p>
+              </div>
+            </div>
+          </div>
+        </article>
+        <div className="footer__social"></div>
+        <div className="footer__copyright"></div>
       </div>
     </footer>
   );
