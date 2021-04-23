f74182db7dc25d0e626d610a3ba213713c8d82cc
Albeiro Marin <xlandru@hotmail.com>
2021-04-23 14:24:12 -0500

    Add the discover section component

---
+++ src/components/Discover section/DiscoverSection.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+
+
+
+const DiscoverSection = () => {
+
+    return (
+        <section className="discover-section">
+
+            <div className="discover-section__card-container">
+                <div className="discover-section__card-subcontainer">
+                    <div className="discover-section__card-flex-container">
+                        <article className="discover-section__card"></article>
+                    </div>
+                </div>
+            </div>
+
+            <div className="discover-section__brand-container">
+                <div className="discover-section__brand-subcontainer"></div>
+            </div>
+
+        </section>
+    )
+};
+
+export default DiscoverSection;
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -3,6 +3,7 @@ import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
 import Slide from '../../components/Slide/Slide';
+import DiscoverSection from '../../components/Discover section/DiscoverSection';
 
 
 
@@ -13,6 +14,7 @@ const Home = () => {
         <div className="home">
 
             <Slide />
+            <DiscoverSection />
             
         </div>
     )
