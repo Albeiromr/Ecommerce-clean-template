be2d3179fd731997291b4dacd7d07064757d948c
Albeiro Marin <xlandru@hotmail.com>
2021-05-03 16:53:54 -0500

    Add the actual-path component

---
+++ src/components/Actual path/ActualPath.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+
+
+const ActualPAth = () => {
+
+    return(
+        <div className="actual-path"></div>
+    )
+};
+
+export default ActualPAth;
+
--- src/routes/Products/Products.tsx
+++ src/routes/Products/Products.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import ActualPath from '../../components/Actual path/ActualPath';
 import DiscoverSection from '../../components/Discover section/DiscoverSection';
 import InterestProducts from '../../components/Interest products/InterestProducts';
+
 
 const Products = () => {
 
     return(
         <div className="products">
+            <ActualPath />
             <DiscoverSection />
             <InterestProducts />
         </div>
