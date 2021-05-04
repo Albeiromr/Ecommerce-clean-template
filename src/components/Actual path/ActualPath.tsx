c6f3e52c21ecd1b019a12eb0f8b4b09ae01fbede
Albeiro Marin <xlandru@hotmail.com>
2021-05-04 11:30:01 -0500

    Finish the actual-path component

--- src/components/Actual path/ActualPath.tsx
+++ src/components/Actual path/ActualPath.tsx
@@ -6,8 +6,19 @@ import "./1025px-1920px.scss";
 
 const ActualPAth = () => {
 
+    
+
     return(
-        <div className="actual-path"></div>
+        <div className="actual-path">
+            <div className="actual-path__subcontainer">
+                <p className="actual-path__text">Home</p>
+                <p className="actual-path__bar">/</p>
+                <p className="actual-path__text">Sneakers and Shoes</p>
+                <p className="actual-path__bar">/</p>
+                <p className="actual-path__text--strong">Qniro A28</p>
+                <p className="actual-path__bar--hidden">/</p>
+            </div>
+        </div>
     )
 };
 
