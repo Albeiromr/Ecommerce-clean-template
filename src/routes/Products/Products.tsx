701e72ad0f2b5d3f3c12980eacdb75fb8b03f2d2
Albeiro Marin <xlandru@hotmail.com>
2021-05-05 08:58:54 -0500

    Add the product-grid components for  two screen sizes

--- src/routes/Products/Products.tsx
+++ src/routes/Products/Products.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import { useWindowWidth } from "@react-hook/window-size";
 import ActualPath from '../../components/Actual path/ActualPath';
 import MainTitle from '../../components/Main title/MainTitle';
+import DesktopProductGrid from '../../components/Desktop product grid/DesktopProductGrid';
+import MobileProductGrid from '../../components/Mobile product grid/MobileProductGrid';
 import DiscoverSection from '../../components/Discover section/DiscoverSection';
 import InterestProducts from '../../components/Interest products/InterestProducts';
 
@@ -11,10 +14,14 @@ import InterestProducts from '../../components/Interest products/InterestProduct
 
 const Products = () => {
 
+    //with this costum hook we can get the window width for conditionally to render some components
+    const windowWidth = useWindowWidth()
+
     return(
         <div className="products">
             <ActualPath />
             <MainTitle />
+            {windowWidth > 599 ? <DesktopProductGrid /> : <MobileProductGrid />}
             <DiscoverSection />
             <InterestProducts />
         </div>
