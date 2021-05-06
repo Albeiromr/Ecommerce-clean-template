bdfad0a47afc9eccd3359d0a493c44d3abeca409
Albeiro Marin <xlandru@hotmail.com>
2021-05-06 11:09:30 -0500

    Finish the individual-category component

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import IndividualCategory from '../Individual category/IndividualCategory';
 
 
 const Filters = () => {
@@ -9,7 +10,15 @@ const Filters = () => {
     return(
         <aside className="filters">
             <h2 className="filters__title-one">Categories</h2>
-            <div className="filters__categories-container"></div>
+
+            <div className="filters__categories-container">
+                <IndividualCategory />
+                <IndividualCategory />
+                <IndividualCategory />
+                <IndividualCategory />
+                <IndividualCategory />
+            </div>
+
             <h2 className="filters__title-two">Filter By Size</h2>
             <div className="filters__sizes-container"></div>
             <div className="filters__range-filter-container"></div>
