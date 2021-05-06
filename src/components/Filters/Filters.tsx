7dd21662a578c3b4c5a9b216f7d7a26b9b74c534
Albeiro Marin <xlandru@hotmail.com>
2021-05-06 09:35:42 -0500

    Add categories and sizes sub containers into the filters component

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -7,8 +7,27 @@ import "./1025px-1920px.scss";
 const Filters = () => {
 
     return(
-        <aside className="filters"></aside>
+        <aside className="filters">
+            <h2 className="filters__title-one">Categories</h2>
+            <div className="filters__categories-container"></div>
+            <h2 className="filters__title-two">Filter By Size</h2>
+            <div className="filters__sizes-container"></div>
+            <div className="filters__range-filter-container"></div>
+            
+            
+        </aside>
     )
-}
+};
+
+/* const individualCategorie = () => {
+
+    return (
+        <div className="individual-categorie">
+
+        </div>
+    )
+}; */
+
+
 
 export default Filters;
