34b98f2fc93f490bf5bf79497516be60afd96766
Albeiro Marin <xlandru@hotmail.com>
2021-05-07 17:42:27 -0500

    Add inputs into the price-filter component

--- src/components/Price filter/PriceFilter.tsx
+++ src/components/Price filter/PriceFilter.tsx
@@ -8,7 +8,40 @@ const PriceFilter = () => {
 
     return (
         <div className="price-filter">
+            <h2 className="price-filter__title">Price</h2>
             <MultiRangeSlider min={1} max={100}/>
+
+            <form className="price-filter__form">
+
+                <div className="price-filter__form-min-container">
+                    <label className="price-filter__form-label" htmlFor="minAmount">Min</label>
+                    <input
+                     className="price-filter__form-input"
+                     type="text" 
+                     name="min"
+                     id="minAmount"
+                    />
+                </div>
+
+                <div className="price-filter__form-dash"/>
+
+                <div className="price-filter__form-max-container">
+                    <label className="price-filter__form-label" htmlFor="maxAmount">Max</label>
+                    <input
+                     className="price-filter__form-input"
+                     type="text" 
+                     name="max"
+                     id="maxAmount"
+                    />
+                </div>
+
+
+            </form>
+
+            <input className="price-filter__submit" type="submit" value="Apply" />
+
+            <p className="price-filter__reset">Reset</p>
+
         </div>
     )
 };
