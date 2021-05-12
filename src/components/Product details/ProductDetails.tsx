190a47c97d1b52ed70a311d4a36923686962e247
Albeiro Marin <xlandru@hotmail.com>
2021-05-12 09:05:14 -0500

    Add the rate indicator into the product-details component

--- src/components/Product details/ProductDetails.tsx
+++ src/components/Product details/ProductDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import fourStars from '../../assets/images/fourStars.png';
 
 
 
@@ -15,6 +16,18 @@ const ProductDetails = () => {
                 <div className="product-details__description-container">
                     <h1 className="product-details__desktop-title">Kamura Orange</h1>
                     <p className="product-details__desktop-sku">SKU 3457SNE9088</p>
+                    <div className="product-details__popularity-container">
+                        <div className="product-details__rate-container">
+                            <p className="product-details__rate-container-number">4.0</p>
+                            <img className="product-details__rate-container-stars" src={fourStars} alt="rate" />
+                            <p className="product-details__rate-container-amount">&#40;223&#41;</p>
+                        </div>
+                        <div className="product-details__popularity-container-line" />
+                        <div className="product-details__sold-container"></div>
+                        <div className="product-details__popularity-container-line product-details__popularity-container-line-last" />
+                        <div className="product-details__viewed-container"></div>
+                        
+                    </div>
                 </div>
             </div>
         </section>
