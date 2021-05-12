3e4c0039c181efbfd3255119ba209c7b8d33760f
Albeiro Marin <xlandru@hotmail.com>
2021-05-11 20:34:31 -0500

    Add title and sku into the product-details component

--- src/components/Product details/ProductDetails.tsx
+++ src/components/Product details/ProductDetails.tsx
@@ -9,7 +9,14 @@ const ProductDetails = () => {
 
     return (
         <section className="product-details">
-
+            <div className="product-details__subcontainer">
+                <h1 className="product-details__mobile-title">Kamura Orange</h1>
+                <p className="product-details__mobile-sku">SKU 3457SNE9088</p>
+                <div className="product-details__description-container">
+                    <h1 className="product-details__desktop-title">Kamura Orange</h1>
+                    <p className="product-details__desktop-sku">SKU 3457SNE9088</p>
+                </div>
+            </div>
         </section>
     )
 };
