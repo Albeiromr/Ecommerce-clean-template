50f392295eda731717c37df68cd49c8669e112f7
Albeiro Marin <xlandru@hotmail.com>
2021-05-25 09:03:07 -0500

    Add Admin-listed-purchased-product component

--- src/components/Admin order details table/AdminOrderDetailsTable.tsx
+++ src/components/Admin order details table/AdminOrderDetailsTable.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import AdminListedPurchasedProduct from '../Admin listed purchased product/AdminListedPurchasedProduct';
 
 const AdminOrderDetailsTable = () => {
 
@@ -51,6 +52,24 @@ const AdminOrderDetailsTable = () => {
                         <p className="admin-order-details-table__second-bar-shipping-title">Shipping Address:</p>
                         <p className="admin-order-details-table__second-bar-shipping-text">Cll 123a # 148b -12</p>
                     </div>
+                </div>
+
+                <div className="admin-order-details-table__third-bar">
+
+                    <p className="admin-order-details-table__third-bar-text-one">Picture</p>
+                    <p className="admin-order-details-table__third-bar-text-two">SKU</p>
+                    <p className="admin-order-details-table__third-bar-text-three">Product Name</p>
+                    <p className="admin-order-details-table__third-bar-text-four">Quantity</p>
+                    <p className="admin-order-details-table__third-bar-text-five">SubTotal</p>
+
+                </div>
+
+                <div className="admin-order-details-table__product-container">
+                    <AdminListedPurchasedProduct />
+                    <AdminListedPurchasedProduct />
+                    <AdminListedPurchasedProduct />
+                    <AdminListedPurchasedProduct />
+                    <AdminListedPurchasedProduct />
                 </div>
 
             </div>
