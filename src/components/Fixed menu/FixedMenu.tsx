9ae91999ed01a9337a7f6d832e215a66707236cb
Albeiro Marin <xlandru@hotmail.com>
2021-06-24 17:20:28 -0500

    Add routing to fixed-menu component

--- src/components/Fixed menu/FixedMenu.tsx
+++ src/components/Fixed menu/FixedMenu.tsx
@@ -3,10 +3,18 @@ import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
 import basket from "../../assets/icons/basket.svg";
+import {useHistory} from 'react-router-dom';
 
 const FixedMenu = () => {
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/shopping-cart');
+  }
+
   return (
-      <div className="fixed-menu">
+      <div onClick={handleClick} className="fixed-menu">
         <img  className="fixed-menu__icon" src={basket} alt="basket" />
         <div className="fixed-menu__point"/>
       </div>
